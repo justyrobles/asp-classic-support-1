68468594eb5720bfdff71a1098e577d4bfa81d96
Justy <robles@bayviewphysicians.com>
2022-05-04 07:34:36 -0400

    add css completion

--- src/completion.ts
+++ src/completion.ts
@@ -1,247 +1,344 @@
-import { languages, CompletionItem, CompletionItemKind, TextDocument, Position, SymbolKind, DocumentSymbol, Uri, commands, MarkdownString } from "vscode";
-import { builtInSymbols, output } from "./extension"
+import {
+	languages,
+	CompletionItem,
+	CompletionItemKind,
+	TextDocument,
+	Position,
+	SymbolKind,
+	DocumentSymbol,
+	Uri,
+	commands,
+	MarkdownString,
+	CompletionList,
+	ExtensionContext,
+	CompletionContext,
+	CancellationToken,
+	workspace,
+} from "vscode";
+import { getLanguageService } from "vscode-html-languageservice";
+import { builtInSymbols, output } from "./extension";
 import * as PATTERNS from "./patterns";
 import { currentDocSymbols, getDocsForLine } from "./symbols";
-import { getRegionsInsideRange, positionIsInsideAspRegion, replaceCharacter } from "./region";
+import {
+	getRegionsInsideRange,
+	positionIsInsideAspRegion,
+	replaceCharacter,
+} from "./region";
+import { getCSSVirtualContent, isInsideStyleRegion } from "./embeddedSupport";
 import { should } from "chai";
 
+const htmlLanguageService = getLanguageService();
+const virtualDocumentContents = new Map<string, string>();
+workspace.registerTextDocumentContentProvider("embedded-content", {
+	provideTextDocumentContent: (uri) => {
+		const originalUri = uri.path.slice(1).slice(0, -4);
+		const decodedUri = decodeURIComponent(originalUri);
+		return virtualDocumentContents.get(decodedUri);
+	},
+});
+
 /** Returns true if an object was found... or something? */
-function getObjectMembersCode(objectsToAdd : CompletionItem[], objectName: string): boolean {
+function getObjectMembersCode(
+	objectsToAdd: CompletionItem[],
+	objectName: string
+): boolean {
+	for (const symbol of [...currentDocSymbols, ...builtInSymbols]) {
+		if (
+			symbol.symbol.kind === SymbolKind.Class &&
+			symbol.symbol.name.toLowerCase() === objectName.toLowerCase()
+		) {
+			for (const item of symbol.symbol.children) {
+				const completion = getCompletionFromSymbol(item);
 
-  for(const symbol of [...currentDocSymbols, ...builtInSymbols]) {
+				const documentation = getDocumentationForSymbol(item, objectName);
 
-    if(symbol.symbol.kind === SymbolKind.Class && symbol.symbol.name.toLowerCase() === objectName.toLowerCase()) {
+				if (documentation) {
+					completion.documentation = new MarkdownString(documentation);
+				}
 
-      for(const item of symbol.symbol.children) {
+				// We've already added it, go on
+				if (
+					objectsToAdd.some(
+						(i) => i.label === completion.label && i.kind == completion.kind
+					)
+				)
+					continue;
 
-        const completion = getCompletionFromSymbol(item);
+				objectsToAdd.push(completion);
+			}
 
-        const documentation = getDocumentationForSymbol(item, objectName);
+			return true;
+		}
+	}
 
-        if(documentation) {
-          completion.documentation = new MarkdownString(documentation);
-        }
-
-        // We've already added it, go on
-        if(objectsToAdd.some(i => i.label === completion.label && i.kind == completion.kind )) continue;
-
-        objectsToAdd.push(completion);
-      }
-
-      return true;
-    }
-  }
-
-  return false;
+	return false;
 }
 
-function getDocumentationForSymbol(symbol: DocumentSymbol, parentName: string): string {
-  for(const aspSymbol of [...currentDocSymbols, ...builtInSymbols]) {
-    if(aspSymbol.symbol.name !== symbol.name || aspSymbol.parentName?.toLowerCase() !== parentName.toLowerCase()) {
-      continue;
-    }
+function getDocumentationForSymbol(
+	symbol: DocumentSymbol,
+	parentName: string
+): string {
+	for (const aspSymbol of [...currentDocSymbols, ...builtInSymbols]) {
+		if (
+			aspSymbol.symbol.name !== symbol.name ||
+			aspSymbol.parentName?.toLowerCase() !== parentName.toLowerCase()
+		) {
+			continue;
+		}
 
-    return aspSymbol.documentation?.summary;
-  }
+		return aspSymbol.documentation?.summary;
+	}
 }
 
 function getCompletionFromSymbol(symbol: DocumentSymbol): CompletionItem {
-  let kind: CompletionItemKind = CompletionItemKind.Variable;
+	let kind: CompletionItemKind = CompletionItemKind.Variable;
 
-  if(symbol.kind === SymbolKind.Variable) kind = CompletionItemKind.Variable;
-  if(symbol.kind === SymbolKind.Property) kind = CompletionItemKind.Property;
-  if(symbol.kind === SymbolKind.Function) kind = CompletionItemKind.Function;
-  if(symbol.kind === SymbolKind.Class) kind = CompletionItemKind.Class;
+	if (symbol.kind === SymbolKind.Variable) kind = CompletionItemKind.Variable;
+	if (symbol.kind === SymbolKind.Property) kind = CompletionItemKind.Property;
+	if (symbol.kind === SymbolKind.Function) kind = CompletionItemKind.Function;
+	if (symbol.kind === SymbolKind.Class) kind = CompletionItemKind.Class;
 
-  return new CompletionItem(symbol.name, kind);
+	return new CompletionItem(symbol.name, kind);
 }
 
-function provideCompletionItems(doc: TextDocument, position: Position): CompletionItem[] {
+async function forwardCSS(
+	document: TextDocument,
+	position: Position,
+	context: CompletionContext
+) {
+	const originalUri = document.uri.toString();
+	virtualDocumentContents.set(
+		originalUri,
+		getCSSVirtualContent(htmlLanguageService, document.getText())
+	);
 
-  const regionTest = positionIsInsideAspRegion(doc, position);
+	const vdocUriString = `embedded-content://css/${encodeURIComponent(
+		originalUri
+	)}.css`;
+	const vdocUri = Uri.parse(vdocUriString);
+	try {
+		const results = await commands.executeCommand<CompletionList>(
+			"vscode.executeCompletionItemProvider",
+			vdocUri,
+			position,
+			context.triggerCharacter
+		);
 
-  // We're not in ASP, exit
-  if(!regionTest.isInsideRegion) {
-    return [];
-  }
+		return results;
+	} catch (error) {
+		output.append(error);
+	}
+}
 
-  const line = doc.lineAt(position);
-  let lineText = line.text;
+async function provideCompletionItems(
+	doc: TextDocument,
+	position: Position,
+	token: CancellationToken,
+	context: CompletionContext
+): Promise<CompletionItem[]> {
+	// If in `<style>`,perform request forwarding
+	if (
+		isInsideStyleRegion(
+			htmlLanguageService,
+			doc.getText(),
+			doc.offsetAt(position)
+		)
+	) {
+		return (await forwardCSS(doc, position, context)).items;
+	}
 
-  // Remove completion offerings from commented lines
-  if (line.text.charAt(line.firstNonWhitespaceCharacterIndex) === "'") {
-    return [];
-  }
+	const regionTest = positionIsInsideAspRegion(doc, position);
 
-  const interiorRegions = getRegionsInsideRange(regionTest.regions, line.range);
+	// We're not in ASP, exit
+	if (!regionTest.isInsideRegion) {
+		return [];
+	}
 
-  if(interiorRegions.length > 0) {
+	const line = doc.lineAt(position);
+	let lineText = line.text;
 
-    for (let index = 0; index < lineText.length; index++) {
-      const characterPosition = new Position(line.lineNumber, index);
+	// Remove completion offerings from commented lines
+	if (line.text.charAt(line.firstNonWhitespaceCharacterIndex) === "'") {
+		return [];
+	}
 
-      let isInsideRegion = false;
+	const interiorRegions = getRegionsInsideRange(regionTest.regions, line.range);
 
-      // is the character inside the region?
-      for(const interiorRegion of interiorRegions) {
-        if(interiorRegion.contains(characterPosition)) {
-          // This character is inside a region
-          isInsideRegion = true;
+	if (interiorRegions.length > 0) {
+		for (let index = 0; index < lineText.length; index++) {
+			const characterPosition = new Position(line.lineNumber, index);
 
-          break;
-        }
-      }
+			let isInsideRegion = false;
 
-      // Blank out the non-ASP stuff in this line
-      if(!isInsideRegion) {
-        lineText = replaceCharacter(lineText, " ", index);
-      }
-    }
-  }
+			// is the character inside the region?
+			for (const interiorRegion of interiorRegions) {
+				if (interiorRegion.contains(characterPosition)) {
+					// This character is inside a region
+					isInsideRegion = true;
 
-  // TODO: sanitize non-ASP code
-  const codeAtPosition = lineText.substring(0, position.character);
+					break;
+				}
+			}
 
-  // No completion during writing a definition, still buggy
-  if (PATTERNS.VAR_COMPLS.test(codeAtPosition)) {
-    return [];
-  }
+			// Blank out the non-ASP stuff in this line
+			if (!isInsideRegion) {
+				lineText = replaceCharacter(lineText, " ", index);
+			}
+		}
+	}
 
-  // No completion within open string
-  let quoteCount = 0;
+	// TODO: sanitize non-ASP code
+	const codeAtPosition = lineText.substring(0, position.character);
 
-  for (const char of codeAtPosition) {
-    if (char === '"') quoteCount++;
-  }
+	// No completion during writing a definition, still buggy
+	if (PATTERNS.VAR_COMPLS.test(codeAtPosition)) {
+		return [];
+	}
 
-  // No completion inside a quote block
-  if (quoteCount % 2 === 1) {
-    return [];
-  }
+	// No completion within open string
+	let quoteCount = 0;
 
-  const results: CompletionItem[] = [];
+	for (const char of codeAtPosition) {
+		if (char === '"') quoteCount++;
+	}
 
-  /**
-   * Matches when the last character typed is a dot
-   *
-   * 1. The word preceding the dot.
-  */
-  const dotTypedMatch = /.*\b(\w+)\.\w*$/.exec(codeAtPosition);
+	// No completion inside a quote block
+	if (quoteCount % 2 === 1) {
+		return [];
+	}
 
-  // DOT typed, only show members!
-  if (dotTypedMatch && dotTypedMatch.length > 0) {
+	const results: CompletionItem[] = [];
 
-    const objectName = dotTypedMatch[1];
+	/**
+	 * Matches when the last character typed is a dot
+	 *
+	 * 1. The word preceding the dot.
+	 */
+	const dotTypedMatch = /.*\b(\w+)\.\w*$/.exec(codeAtPosition);
 
-    output.appendLine(`Dot typed for object: ${objectName}`);
+	// DOT typed, only show members!
+	if (dotTypedMatch && dotTypedMatch.length > 0) {
+		const objectName = dotTypedMatch[1];
 
-    // eslint-disable-next-line no-empty
-    if (getObjectMembersCode(results, objectName)) {
+		output.appendLine(`Dot typed for object: ${objectName}`);
 
-    }
-    else {
-
+		// eslint-disable-next-line no-empty
+		if (getObjectMembersCode(results, objectName)) {
+		} else {
 			// Fall back to using all available symbols
-			for(const symbol of [...currentDocSymbols, ...builtInSymbols]) {
-
+			for (const symbol of [...currentDocSymbols, ...builtInSymbols]) {
 				const completion = getCompletionFromSymbol(symbol.symbol);
 
-				if(!symbol.isTopLevel) continue;
+				if (!symbol.isTopLevel) continue;
 
-        if(symbol.documentation && symbol.documentation.summary) {
-          completion.documentation = new MarkdownString(symbol.documentation.summary);
-        }
+				if (symbol.documentation && symbol.documentation.summary) {
+					completion.documentation = new MarkdownString(
+						symbol.documentation.summary
+					);
+				}
 
-				if(results.some(i => i.label === completion.label && i.kind == completion.kind )) continue;
+				if (
+					results.some(
+						(i) => i.label === completion.label && i.kind == completion.kind
+					)
+				)
+					continue;
 
 				results.push(completion);
 			}
-
-    }
-  }
-  else {
-
-    // TODO: get context (what class or function are we in?)
-    const scope = getScope();
+		}
+	} else {
+		// TODO: get context (what class or function are we in?)
+		const scope = getScope();
 
 		// TODO: don't factor out this and above as it's copy/paste
 		// No DOT, use all available symbols
-    for(const symbol of [...currentDocSymbols, ...builtInSymbols]) {
-
+		for (const symbol of [...currentDocSymbols, ...builtInSymbols]) {
 			const completion = getCompletionFromSymbol(symbol.symbol);
 
-      // if the symbol is not top level or does not live in this context, skip it
-      if(!symbol.isTopLevel && symbol.parentName.toLowerCase() !== scope?.toLowerCase()) {
-        continue;
-      }
+			// if the symbol is not top level or does not live in this context, skip it
+			if (
+				!symbol.isTopLevel &&
+				symbol.parentName.toLowerCase() !== scope?.toLowerCase()
+			) {
+				continue;
+			}
 
-      if(symbol.documentation && symbol.documentation.summary) {
-        completion.documentation = new MarkdownString(symbol.documentation.summary);
-      }
+			if (symbol.documentation && symbol.documentation.summary) {
+				completion.documentation = new MarkdownString(
+					symbol.documentation.summary
+				);
+			}
 
-      // Do we already have this completion in our results? Skip.
-			if(results.some(i => i.label === completion.label && i.kind == completion.kind )) continue;
+			// Do we already have this completion in our results? Skip.
+			if (
+				results.some(
+					(i) => i.label === completion.label && i.kind == completion.kind
+				)
+			)
+				continue;
 
-      results.push(completion);
-    }
-  }
+			results.push(completion);
+		}
+	}
 
-  return results;
+	return results;
 
-  function getScope(): string {
+	function getScope(): string {
+		let shouldSearchFunctionScope = true;
 
-    let shouldSearchFunctionScope = true;
+		for (let index = position.line - 1; index >= 0; index--) {
+			const currentLine = doc.lineAt(index);
 
-    for (let index = position.line - 1; index >= 0; index--) {
-      const currentLine = doc.lineAt(index);
+			if (currentLine.isEmptyOrWhitespace) {
+				continue;
+			}
 
-      if(currentLine.isEmptyOrWhitespace) {
-        continue;
-      }
+			if (
+				currentLine.text[currentLine.firstNonWhitespaceCharacterIndex] === "'"
+			) {
+				continue;
+			}
 
-      if(currentLine.text[currentLine.firstNonWhitespaceCharacterIndex] === "'") {
-        continue;
-      }
+			// We're outside of a class, hence in the global scope
+			if (currentLine.text.toLowerCase().indexOf("end class") > -1) {
+				return null;
+			}
 
-      // We're outside of a class, hence in the global scope
-      if(currentLine.text.toLowerCase().indexOf("end class") > -1) {
-        return null;
-      }
+			if (currentLine.text.toLowerCase().indexOf("end function") > -1) {
+				shouldSearchFunctionScope = false;
+			}
 
-      if(currentLine.text.toLowerCase().indexOf("end function") > -1) {
-        shouldSearchFunctionScope = false;
-      }
+			if (shouldSearchFunctionScope) {
+				var functionMatch = PATTERNS.FUNCTION.exec(currentLine.text);
 
-      if(shouldSearchFunctionScope) {
-        var functionMatch = PATTERNS.FUNCTION.exec(currentLine.text);
+				if (functionMatch && functionMatch[5]) {
+					const name = functionMatch[5];
 
-        if(functionMatch && functionMatch[5]) {
-          const name = functionMatch[5];
+					output.appendLine(`Scope found: ${name}`);
 
-          output.appendLine(`Scope found: ${name}`);
+					return name;
+				}
+			}
 
-          return name;
-        }
-      }
+			var classMatch = PATTERNS.CLASS.exec(currentLine.text);
 
-      var classMatch = PATTERNS.CLASS.exec(currentLine.text);
+			if (classMatch && classMatch[3]) {
+				const name = classMatch[3];
 
-      if(classMatch && classMatch[3]) {
-        const name = classMatch[3];
+				output.appendLine(`Scope found: ${name}`);
 
-        output.appendLine(`Scope found: ${name}`);
+				return name;
+			}
+		}
 
-        return name;
-      }
-
-    }
-
-    // Didn't find anything -- global scope
-    return null;
-  }
+		// Didn't find anything -- global scope
+		return null;
+	}
 }
 
 export default languages.registerCompletionItemProvider(
-  { scheme: "file", language: "asp" },
-  { provideCompletionItems }, "."
+	{ scheme: "file", language: "asp" },
+	{ provideCompletionItems },
+	"."
 );
